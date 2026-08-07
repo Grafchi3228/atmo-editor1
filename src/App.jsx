79fe3cc8dd3902388fa58d91765d07eb71296e9e
Volodymyr <vovapetryshyn99@gmail.com>
2026-08-08 00:12:18 +0300

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoEditor from './VideoEditor';
+
+function App() {
+  return (
+    <div className="w-screen h-screen m-0 p-0 overflow-hidden bg-slate-50">
+      <VideoEditor />
+    </div>
+  );
+}
+
+export default App;
